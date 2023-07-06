41b45b16fcd755f612a3af143b855f31734839bb
DavidG <46117427+drthnox@users.noreply.github.com>
2023-07-06 23:22:56 +0800

    Adding env configs for each server

--- blockchain_server/main.go
+++ blockchain_server/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"flag"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"log"
+	"path/filepath"
+	"strings"
 )
 
 var sugar *zap.SugaredLogger
+var config *viper.Viper
 
 func init() {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync() // flushes buffer, if any
-	sugar = logger.Sugar()
-	sugar.Named("BlockchainServer")
+
 }
 
 func main() {
-	port := flag.Uint("port", 6000, "TCP Port Number for Blockchain Server")
+	absPath, err := filepath.Abs("./blockchain_server")
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the absolute path of the file
+	log.Printf("=====> %s", absPath)
+	config := viper.New()
+	file := filepath.Join(absPath, ".env")
+	config.SetConfigFile(file)
+	config.SetConfigType("json")
+	if err := config.ReadInConfig(); err != nil {
+		log.Fatalf("Error to reading config file, %s", err)
+	}
+	env := config.GetString("env")
+	logger, _ := zap.NewProduction()
+	if strings.ToLower(env) == "dev" {
+		logger, _ = zap.NewDevelopment()
+	}
+	defer logger.Sync() // flushes buffer, if any
+	sugar = logger.Sugar()
+	sugar.Named("BlockchainServer")
+	port := flag.Uint("port", config.GetUint("port"), "TCP Port Number for Blockchain Server")
 	flag.Parse()
 	app := NewBlockchainServer(uint16(*port))
 	app.Run()
--- wallet_server/main.go
+++ wallet_server/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"flag"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"log"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 var sugar *zap.SugaredLogger
 
 func init() {
+
 	logger, _ := zap.NewProduction()
 	defer logger.Sync() // flushes buffer, if any
 	sugar = logger.Sugar()
@@ -15,9 +21,32 @@ func init() {
 }
 
 func main() {
-	port := flag.Uint("port", 8080, "TCP Port Number for Wallet Server")
-	gateway := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
+	absPath, err := filepath.Abs("./wallet_server")
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the absolute path of the file
+	log.Printf("=====> %s", absPath)
+	config := viper.New()
+	file := filepath.Join(absPath, ".env")
+	config.SetConfigFile(file)
+	config.SetConfigType("json")
+	if err := config.ReadInConfig(); err != nil {
+		log.Fatalf("Error to reading config file, %s", err)
+	}
+	env := config.GetString("env")
+	logger, _ := zap.NewProduction()
+	if strings.ToLower(env) == "dev" {
+		logger, _ = zap.NewDevelopment()
+	}
+	defer logger.Sync() // flushes buffer, if any
+	sugar = logger.Sugar()
+	sugar.Named("WalletServer")
+	port := *flag.Int("port", config.GetInt("port"), "TCP Port Number for Wallet Server")
+	s := "http://127.0.0.1:" + strconv.Itoa(port)
+	gateway := flag.String("gateway", "http://127.0.0.1:"+s, "Blockchain Gateway")
 	flag.Parse()
-	app := NewWalletServer(uint16(*port), *gateway)
+	app := NewWalletServer(uint16(port), *gateway)
 	app.Run()
 }
